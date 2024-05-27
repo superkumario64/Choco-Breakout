7b1f868bf7695fcc427d67a1d7b17d2ed5a5be8f
Kumar Agarwal <superkumario64@gmail.com>
2024-05-27 08:39:26 -0700

    Initial commit of Choco Breakout plugin files. WP scaffold files

---
+++ choco-breakout.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Choco Breakout
+ * Plugin URI:      https://github.com/superkumario64/Choco-Breakout
+ * Description:     Choco Themed Breakout Game
+ * Author:          Choco Del Mar
+ * Author URI:      https://chocodelmar.com
+ * Text Domain:     choco-breakout
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Choco_Breakout
+ */
+
+// Your code starts here.
